4acd86ca8f6359468c008af9b6aa1a9e5cae3af4
Derek Garcia <dgarcia2@hawaii.edu>
2025-02-07 16:30:39 -1000

    docs: add crawler docstrings

--- py-crawler/graven/crawl/crawler.py
+++ py-crawler/graven/crawl/crawler.py
@@ -13,8 +13,8 @@ from aiohttp import ClientSession, TCPConnector
 
 from log.logger import logger
 
-DEFAULT_MAX_CONCURRENT_REQUESTS = 10
 DEFAULT_MAX_RETRIES = 3
+DEFAULT_MAX_CONCURRENT_REQUESTS = 10
 
 # todo - update to exclude javadocs, sources, etc
 MAVEN_HTML_REGEX = re.compile(
@@ -22,55 +22,84 @@ MAVEN_HTML_REGEX = re.compile(
 
 
 class CrawlerWorker:
-    def __init__(self, download_queue: Queue, max_retries: int,
+    def __init__(self, download_queue: Queue, max_retries: int = DEFAULT_MAX_RETRIES,
                  max_concurrent_requests: int = DEFAULT_MAX_CONCURRENT_REQUESTS):
+        """
+        Create a new crawler worker that asynchronously and recursively parses the maven central file tree
+
+        :param download_queue: Queue to add urls of jars to download to
+        :param max_retries: Max number of retries to get a url from the crawl queue before exiting (default: 3)
+        :param max_concurrent_requests: Max number of concurrent requests allowed to be made at once (default: 10)
+        """
         self._crawl_queue = Queue()
         self._download_queue = download_queue
         self._max_retries = max_retries
         self._semaphore = Semaphore(max_concurrent_requests)
 
-    async def _process_html(self, url: str, html: str) -> None:
+    async def _parse_html(self, url: str, html: str) -> None:
+        """
+        Parse the html and add new crawl and download urls to their respective queues
 
+        :param url: URL source of html
+        :param html: html content
+        """
+        # parse each match in the html
         for match in re.finditer(MAVEN_HTML_REGEX, html):
-            # new dir
+            # new crawl url
             if match.group(1):
                 crawl_url = f"{url}{match.group(1)}"
                 await self._crawl_queue.put(crawl_url)
                 logger.debug_msg(f"Found crawl url | {crawl_url}")
                 continue
-            # save jar url and timestamp
+            # new download url
             if match.group(2):
                 download_url = f"{url}{match.group(2)}"
-                await self._download_queue.put((download_url, match.group(3)))
+                await self._download_queue.put((download_url, match.group(3)))  # save jar url and timestamp
                 logger.debug_msg(f"Found jar url | {download_url}")
 
     async def _crawl(self, session: ClientSession) -> None:
+        """
+        Main crawl method. Will continuously download and parse urls until the crawl urls is empty and retries exceeded
+
+        :param session: aiohttp session to use for requesting htmls
+        """
         cur_retries = 0
         while True:
             try:
+                # If the queue is empty, will error
                 url = self._crawl_queue.get_nowait()
-
+                # download html
                 async with self._semaphore:
                     async with session.get(url) as response:
                         response.raise_for_status()  # todo handle
                         html = await response.text()
-
-                await self._process_html(url, html)
+                # update queues and continue
+                await self._parse_html(url, html)
                 self._crawl_queue.task_done()
                 cur_retries = 0
             except asyncio.QueueEmpty:
-                if cur_retries > self._max_retries:
+                # exit if exceed retries
+                if cur_retries >= self._max_retries:
                     logger.warn(f"No urls left in crawl queue, exiting. . .")
                     break
-                logger.warn(f"No urls left in crawl queue, retrying ({cur_retries}/{self._max_retries}). . .")
+                # sleep and try again
                 cur_retries += 1
+                logger.warn(f"No urls left in crawl queue, retrying ({cur_retries}/{self._max_retries}). . .")
                 await asyncio.sleep(1)  # todo - might need to increase?
 
     async def start(self, root_url: str) -> None:
+        """
+        Launch the crawler
+
+        :param root_url: Root url to start the crawler at
+        """
+        # init crawler
         start_time = time.time()
         await self._crawl_queue.put(root_url)
         logger.info(f"Starting crawler at '{root_url}'")
+        # crawl until no urls left
         async with ClientSession(connector=TCPConnector(limit=50)) as session:
             await self._crawl(session)
             await self._crawl_queue.join()
+
         logger.info(f"Completed crawl in {time.time() - start_time:.2f} seconds")
